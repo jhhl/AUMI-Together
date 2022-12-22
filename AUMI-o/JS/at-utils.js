ce6ad0a31307862d138a4c7f47aa82f11e65077d
Jhh Lowengard <github@jhhl.net>
2022-12-22 12:02:49 -0500

    at-utilities set up, part of the split up of the much too messy index.html

---
+++ AUMI-o/JS/at-utils.js
@@ -0,0 +1,2 @@
+// general use utilities
+// this will more useful when service workers are running and will need their own non-shared codes
---
+++ AUMI-o/modules/at-inst.js
@@ -0,0 +1,4 @@
+// AUMI together instrument code
+// AUDIO playing and MIDI playing should be here.
+// provides:
+// the manifest
